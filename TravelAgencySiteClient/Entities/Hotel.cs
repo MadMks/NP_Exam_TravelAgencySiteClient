2db439b9ac321cf4fb721b42953b4fa31dccd143
Makc <mks59k@gmail.com>
2019-01-26 21:51:22 +0200

    Добавлен класс Hotel.

---
+++ TravelAgencySiteClient/Entities/Hotel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelAgencySiteClient.Entities
+{
+    public class Hotel
+    {
+        public int id { get; set; }
+        public string hotelName { get; set; }
+        public int stars { get; set; }
+        public int cost { get; set; }
+        public string info { get; set; }
+    }
+}
