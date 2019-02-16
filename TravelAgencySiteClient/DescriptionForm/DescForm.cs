f99c4776f3f25f6349f12bb7daf2d9e8b3746575
Makc <mks59k@gmail.com>
2019-02-16 22:37:37 +0200

    Создание окна для вывода описания отеля. Визуальное оформление.

---
+++ TravelAgencySiteClient/DescriptionForm/DescForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TravelAgencySiteClient.DescriptionForm
+{
+    public partial class DescForm : Form
+    {
+        public DescForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
